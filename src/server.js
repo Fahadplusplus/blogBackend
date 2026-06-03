a61b6509804a03400961c4b36a0be1216317e9e6
Fahadplusplus <nasirfahad420@gmail.com>
2026-06-03 15:38:32 +0500

    allowed all eddpoints

--- src/server.js
+++ src/server.js
@@ -1,47 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const gitData = {
-  "login": "Fahadplusplus",
-  "id": 186282082,
-  "node_id": "U_kgDOCxpwYg",
-  "avatar_url": "https://avatars.githubusercontent.com/u/186282082?v=4",
-  "gravatar_id": "",
-  "url": "https://api.github.com/users/Fahadplusplus",
-  "html_url": "https://github.com/Fahadplusplus",
-  "followers_url": "https://api.github.com/users/Fahadplusplus/followers",
-  "following_url": "https://api.github.com/users/Fahadplusplus/following{/other_user}",
-  "gists_url": "https://api.github.com/users/Fahadplusplus/gists{/gist_id}",
-  "starred_url": "https://api.github.com/users/Fahadplusplus/starred{/owner}{/repo}",
-  "subscriptions_url": "https://api.github.com/users/Fahadplusplus/subscriptions",
-  "organizations_url": "https://api.github.com/users/Fahadplusplus/orgs",
-  "repos_url": "https://api.github.com/users/Fahadplusplus/repos",
-  "events_url": "https://api.github.com/users/Fahadplusplus/events{/privacy}",
-  "received_events_url": "https://api.github.com/users/Fahadplusplus/received_events",
-  "type": "User",
-  "user_view_type": "public",
-  "site_admin": false,
-  "name": null,
-  "company": null,
-  "blog": "",
-  "location": null,
-  "email": null,
-  "hireable": null,
-  "bio": null,
-  "twitter_username": null,
-  "public_repos": 20,
-  "public_gists": 0,
-  "followers": 2,
-  "following": 3,
-  "created_at": "2024-10-25T09:57:06Z",
-  "updated_at": "2026-05-12T10:59:43Z"
-}
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
+
+const gitData = {
+  login: "Fahadplusplus",
+  id: 186282082,
+  avatar_url: "https://avatars.githubusercontent.com/u/186282082?v=4",
+  html_url: "https://github.com/Fahadplusplus",
+  type: "User",
+  public_repos: 20,
+  followers: 2,
+  following: 3,
+  created_at: "2024-10-25T09:57:06Z",
+  updated_at: "2026-05-12T10:59:43Z"
+};
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -55,9 +40,10 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-app.get("/user",(req ,res)=>{
-  res.json(gitData)
-})
+
+app.get("/user", (req, res) => {
+  res.json(gitData);
+});
 
 const PORT = process.env.PORT || 5000;
 
